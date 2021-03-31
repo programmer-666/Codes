477ee5e8943db8b272ba653dcd62bfb01e691a76
Süha <suharslan@protonmail.ch>
2021-03-31 22:33:33 +0300

    Delete sendText.py

--- Python/Discord/sendText.py
+++
@@ -1,9 +0,0 @@
-import discord
-from discord.ext import commands
-
-cl = commands.Bot(command_prefix='§')
-
-@cl.event
-async def on_ready(ctx):
-    await ctx.send('gençler iyi akşamlar')
-cl.run('ODIxODUzODExMjA2NTIwODUy.YFJxAQ.p7mwhYBja4W1EGywJ8AH6KOv0u8')
